812a2cdefd2ae43f41c03862b875f00f8ace2ce6
DESKTOP-TP0PGUH\jeffe <jeffev123@gmail.com>
2024-05-26 15:27:03 -0300

    adicionado scripts

---
+++ Scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clona os repositórios
+git clone https://github.com/jeffev/investlink_backend.git
+git clone https://github.com/jeffev/investlink_frontend.git
+git clone https://github.com/jeffev/investlink_data_science_pipeline.git
+
+# Inicia o docker-compose
+docker-compose up --build
